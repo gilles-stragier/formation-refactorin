92c5d5f6c0b5ced4d6a67ff23bb8d2cb495b8e5f
gilles.stragier <gilles.stragier@formatech.be>
2019-07-29 16:44:25 +0200

    * Extracted Method isBackstagePass

--- src/main/java/be/formatech/training/formationrefactoring/gildedrose/GildedRose.java
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/GildedRose.java
@@ -9,8 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!isAgedBrie(items[i])
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!isAgedBrie(items[i]) && !isBackstagePass(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -20,7 +19,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePass(items[i])) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -42,7 +41,7 @@ class GildedRose {
 
             if (items[i].sellIn < 0) {
                 if (!isAgedBrie(items[i])) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!isBackstagePass(items[i])) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
@@ -62,5 +61,9 @@ class GildedRose {
 
     private boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
