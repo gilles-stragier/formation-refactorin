5869ac0b46e69dc748a935195c66b8dea5b1ce23
gilles.stragier <gilles.stragier@formatech.be>
2019-07-29 17:32:06 +0200

    * Everything moved to calculator

--- src/main/java/be/formatech/training/formationrefactoring/gildedrose/GildedRose.java
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/GildedRose.java
@@ -1,7 +1,6 @@
 package be.formatech.training.formationrefactoring.gildedrose;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,88 +20,9 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        wrappedItems.forEach(this::updateQualityPerItem);
+        wrappedItems.forEach(c -> c.updateQualityPerItem());
     }
 
-    private void updateQualityPerItem(ItemCalculator itemCalculator) {
-        computeQuality(itemCalculator.item);
-        computeSellIn(itemCalculator.item);
-        handleExpiration(itemCalculator.item);
-    }
-
-    void handleExpiration(Item item) {
-        if (isExpired(item)) {
-            if (!isAgedBrie(item)) {
-                if (!isBackstagePass(item)) {
-                    decreaseQuality(item);
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-    }
-
-    void computeSellIn(Item item) {
-        if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    void computeQuality(Item item) {
-        if (!isAgedBrie(item) && !isBackstagePass(item)) {
-            decreaseQuality(item);
-        } else {
-            increaseQuality(item);
-        }
-    }
-
-    void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (isBackstagePass(item)) {
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
-        }
-    }
-
-    void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            if (!isSulfuras(item)) {
-                item.quality = item.quality - 1;
-            }
-        }
-    }
-
-    boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
 
 
 }
--- src/main/java/be/formatech/training/formationrefactoring/gildedrose/ItemCalculator.java
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/ItemCalculator.java
@@ -7,4 +7,85 @@ public class ItemCalculator {
     public ItemCalculator(Item item) {
         this.item = item;
     }
+
+    void updateQualityPerItem() {
+        computeQuality(item);
+        computeSellIn(item);
+        handleExpiration(item);
+    }
+
+    void handleExpiration(Item item) {
+        if (isExpired(item)) {
+            if (!isAgedBrie(item)) {
+                if (!isBackstagePass(item)) {
+                    decreaseQuality(item);
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
+
+    void computeSellIn(Item item) {
+        if (!isSulfuras(item)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    void computeQuality(Item item) {
+        if (!isAgedBrie(item) && !isBackstagePass(item)) {
+            decreaseQuality(item);
+        } else {
+            increaseQuality(item);
+        }
+    }
+
+    void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (isBackstagePass(item)) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            if (!isSulfuras(item)) {
+                item.quality = item.quality - 1;
+            }
+        }
+    }
+
+    boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+    boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
 }
