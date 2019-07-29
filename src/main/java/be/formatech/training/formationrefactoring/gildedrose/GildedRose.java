bf94652a9bd4ea670e3c44e4cac2e57dc9591c4e
gilles.stragier <gilles.stragier@formatech.be>
2019-07-29 17:24:18 +0200

    * Introduced a perfectly useless calculator that right now does nothing

--- src/main/java/be/formatech/training/formationrefactoring/gildedrose/GildedRose.java
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/GildedRose.java
@@ -1,14 +1,23 @@
 package be.formatech.training.formationrefactoring.gildedrose;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 
 class GildedRose {
     Item[] items;
+    private List<ItemCalculator> wrappedItems;
 
     public GildedRose(Item[] items) {
         this.items = items;
+        this.wrappedItems = initCalculators(items);
+    }
+
+    private List<ItemCalculator> initCalculators(Item[] items) {
+        return Arrays.stream(items).map(i -> new ItemCalculator(i)).collect(Collectors.toList());
     }
 
     public void updateQuality() {
---
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/ItemCalculator.java
@@ -0,0 +1,10 @@
+package be.formatech.training.formationrefactoring.gildedrose;
+
+public class ItemCalculator {
+
+    public Item item;
+
+    public ItemCalculator(Item item) {
+        this.item = item;
+    }
+}
