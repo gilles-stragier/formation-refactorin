1cb8babdefbcb8f0cadbfacec5fb24b75bcc3ad4
gilles.stragier <gilles.stragier@formatech.be>
2019-07-29 16:29:11 +0200

    Added a test for the new feature "Conjured stuff"

--- src/test/java/be/formatech/training/formationrefactoring/gildedrose/GildedRoseTest.java
+++ src/test/java/be/formatech/training/formationrefactoring/gildedrose/GildedRoseTest.java
@@ -1,14 +1,14 @@
 package be.formatech.training.formationrefactoring.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     @Test
     public void originalTests() {
-        Item[] items = new Item[] {
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -17,8 +17,7 @@ public class GildedRoseTest {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+        };
 
         GildedRose app = new GildedRose(items);
 
@@ -55,6 +54,23 @@ public class GildedRoseTest {
         assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[7].name);
         assertEquals(4, app.items[7].sellIn);
         assertEquals(50, app.items[7].quality);
+
+    }
+
+
+    @Test
+    public void newFeatureTest() {
+        Item[] items = new Item[]{
+                new Item("Conjured Mana Cake", 3, 6)};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+
+        assertEquals("Conjured Mana Cake", app.items[0].name);
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
 
     }
 
