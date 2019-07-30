41800de256f7335d97ff6b1bc52cd6055f09bb90
gilles.stragier <gilles.stragier@formatech.be>
2019-07-30 14:05:12 +0200

    * Replace conditional logic with polymorphism (expiration) : backstagepasscalculator is there

---
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/BackstagePassCalculator.java
@@ -0,0 +1,8 @@
+package be.formatech.training.formationrefactoring.gildedrose;
+
+public class BackstagePassCalculator extends ItemCalculator {
+
+    public BackstagePassCalculator(Item item) {
+        super(item);
+    }
+}
--- src/main/java/be/formatech/training/formationrefactoring/gildedrose/ItemCalculatorFactory.java
+++ src/main/java/be/formatech/training/formationrefactoring/gildedrose/ItemCalculatorFactory.java
@@ -6,6 +6,8 @@ public class ItemCalculatorFactory {
         switch (item.name) {
             case "Aged Brie":
                 return new AgedBrieCalculator(item);
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new BackstagePassCalculator(item);
             default:
                 return new ItemCalculator(item);
         }
